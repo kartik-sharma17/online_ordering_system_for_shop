08bae3533c4f45b3869aa2c62cde3e6ddc619ee7
kartik sharma <kartiksharma88103@gmail.com>
2024-04-20 17:46:11 +0530

    add slider in index page

--- INDEX.js
+++ INDEX.js
@@ -1,1 +1,19 @@
-// me
+
+
+// for slider 
+
+let slider = document.querySelectorAll(".slide");
+
+
+slider.forEach((slide , index)=>{
+    slide.style.left = `${index * 100}%`;
+})
+
+slider[0].style.transform = translateX(100%);
+
+// timer(()=>{
+//     for(let i = 0 ; i<4 ; i++){
+        
+//     }
+// })
+
